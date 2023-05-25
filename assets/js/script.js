75e405e2ed1e1e3a9b64fec9c7f0b4f79eb40fe7
KseniaSOS <>
2023-05-25 08:23:23 +0000

    Display addidtion question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,6 +60,10 @@ function incrementWrongAnswer() {
 
 function displayAdditionQuestion() {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
 }
 
 function displaySubtractQuestion() {
